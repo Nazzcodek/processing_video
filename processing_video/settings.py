37038c9ae7e9f77e7e581114514b137917a6d1b8
Nazzcodek <bellnas09@gmail.com>
2023-10-02 08:54:55 +0100

    settings, allowed host

--- processing_video/settings.py
+++ processing_video/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
-ALLOWED_HOST = ['processing-video.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+
 
 # Application definition
 
